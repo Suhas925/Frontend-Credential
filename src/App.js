9d832294bb67250e0be7a3eadf7c7ef0807bafe4
Suhas <suhassaibadam@gmail.com>
2024-06-11 14:53:35 +0530

    Created Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './components/Groups/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Credential Frontend Development</h1>
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/Groups/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../../styles/Groups/Navbar.css';
+// import logo from '../assets/favicon.svg';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="left-section">
+          <div className="logo">
+            <i className="bi bi-image-fill"></i>
+            <span>Logoipsum</span>
+          </div>
+          <div className="search">
+            <i className="bi bi-search"></i>
+            <input type="text" placeholder="Search..."/>
+          </div>
+        </div>
+        <div className="right-section">
+          <i class="bi bi-chat-left-text"></i>
+          <i class="bi bi-bell"></i>
+          <i class="bi bi-person-circle"></i>
+          <span>Karlie Brooks</span>
+          <i class="bi bi-chevron-down"></i>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
